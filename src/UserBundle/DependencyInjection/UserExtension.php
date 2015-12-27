3ba64ab83a495a3f61eff0800d280e2e4a7f0561
RafalJaworski <admin@lux-price.pl>
2015-12-27 20:21:25 +0100

    routing changed

--- src/UserBundle/DependencyInjection/UserExtension.php
+++ src/UserBundle/DependencyInjection/UserExtension.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace UserBundle\DependencyInjection;
+
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -10,12 +13,14 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  * Date: 27.12.15
  * Time: 18:12
  */
+
+
 class UserExtension extends Extension //can be extend by Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         //get copy of container instance and after load, this copy is merged to container
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__, '/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__. '/../Resources/config'));
         $loader->load('services.xml');
     }
 }
