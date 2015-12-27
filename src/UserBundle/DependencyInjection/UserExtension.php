ffc3188745f2226ed257638088238348822510e2
RafalJaworski <admin@lux-price.pl>
2015-12-27 18:26:40 +0100

    load configuration implemented

--- src/UserBundle/DependencyInjection/UserExtension.php
+++ src/UserBundle/DependencyInjection/UserExtension.php
@@ -1,5 +1,8 @@
 <?php
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * Created by PhpStorm.
@@ -7,7 +10,29 @@ use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
  * Date: 27.12.15
  * Time: 18:12
  */
-class UserExtension implements ExtensionInterface
+class UserExtension implements ExtensionInterface //can be extend by Extension
 {
 
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        //get copy of container instance and after load, this copy is merged to container
+        $loader = new XmlFileLoader($container,new FileLocator(__DIR__,'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+
+    public function getNamespace()
+    {
+        // TODO: Implement getNamespace() method.
+    }
+
+   c function getXsdValidationBasePath()
+    {
+        // TODO: Implement getXsdValidationBasePath() method.
+    }
+
+
+    public function getAlias()
+    {
+        // TODO: Implement getAlias() method.
+    }
 }
