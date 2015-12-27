6387277626eb74bbc16be5d62a03d25110f84276
RafalJaworski <admin@lux-price.pl>
2015-12-27 18:14:56 +0100

    basic UserExtension

---
+++ src/UserBundle/DependencyInjection/UserExtension.php
@@ -0,0 +1,13 @@
+<?php
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * Created by PhpStorm.
+ * User: jawa
+ * Date: 27.12.15
+ * Time: 18:12
+ */
+class UserExtension implements ExtensionInterface
+{
+
+}
